a577e4bc10d23d322227e0824a517c90f903500e
Ji Hoon <gloryhoon@gmail.com>
2022-06-25 17:18:13 +0900

    Project 초기화

---
+++ src/main/java/com/jihoon/market/JihoonMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jihoon.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihoonMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JihoonMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihoon/market/JihoonMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihoon.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihoonMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

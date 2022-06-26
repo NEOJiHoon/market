5501899e1fd0f7c668dd3adf2e72a407b6f471c0
Ji Hoon <gloryhoon@gmail.com>
2022-06-26 16:58:00 +0900

    insert member 기능 추가

--- src/main/java/com/jihoon/market/mapper/MemberMapper.java
+++ src/main/java/com/jihoon/market/mapper/MemberMapper.java
@@ -1,7 +1,9 @@
 package com.jihoon.market.mapper;
 
+import com.jihoon.market.model.Member;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
+    int insertMember(Member member);
 }

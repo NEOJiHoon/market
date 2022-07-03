7ad09dcc4fc487fd8cab5f0479fd0ac13bfc8c17
Ji Hoon <gloryhoon@gmail.com>
2022-07-03 15:47:03 +0900

    ITEM 컨트롤러 REST방식으로 변경

--- src/main/java/com/jihoon/market/controller/ItemController.java
+++ src/main/java/com/jihoon/market/controller/ItemController.java
@@ -4,33 +4,31 @@ import com.jihoon.market.mapper.ItemMapper;
 import com.jihoon.market.model.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
+@RequestMapping("/item")
 @RestController
 public class ItemController {
 
     @Autowired
     ItemMapper itemMapper;
 
-    @PostMapping(value = "/item/create")
+    @PostMapping()
     public int createItem(Item item) {
         log.info("create item: {}", item);
         return itemMapper.insertItem(item);
     }
 
-    @PostMapping(value="/item/modify")
+    @PutMapping()
     public int modifyItem(Item item) {
         log.info("modify item: {}", item);
         return itemMapper.updateItem(item);
     }
 
-    @PostMapping(value="/item/delete")
+    @DeleteMapping()
     public int deleteItem(@RequestParam String memId, @RequestParam Long itemNo) {
         log.info("::키:: 멤버ID:{}, 아이템번호:{}", memId, itemNo);
         Item item = new Item();
@@ -39,7 +37,7 @@ public class ItemController {
         return itemMapper.deleteItem(item);
     }
 
-    @GetMapping(value="/item")
+    @GetMapping()
     public List<Item> getItemList() {
         List<Item> itemList = itemMapper.selectItemList();
         log.info(":: 아이템 목록 :: {}", itemList);

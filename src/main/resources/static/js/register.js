27a748c6e96f545ab3e5c4d43139a305448c2762
Ji Hoon <gloryhoon@gmail.com>
2022-08-14 17:33:10 +0900

    회원가입 페이지 새로운 디자인 적용

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,46 @@
+function join() {
+    var params ={
+        memId: $('#memId').val(),
+        memNm: $('#memNm').val(),
+        nicNm: $('#nicNm').val(),
+        memPw: $('#memPw').val(),
+        memCp: $('#memCp').val()
+    }
+    console.log('join', params);
+    var memPwRepeat = $("#memPwRepeat").val();
+    if (memPwRepeat !== params.memPw) {
+        alert("입력한 패스워드가 같지 않습니다.");
+        return;
+    }
+
+    $.ajax({
+        type : "POST",
+        url : "/member",
+        contentType : "application/json",
+        data :JSON.stringify(params),
+        success : function(data){
+            console.log('success', data); // Data는 서버에서 준 데이터
+            if (data === -2) {
+                alert("아이디가 중복됐습니다. 다른 아이디로 가입해주세요.");
+            } else if (data === 1) {
+                alert("가입이 완료되었습니다.");
+                location.href = '/';
+            } else if (data === -3) {
+                alert("ID를 입력해 주세요.");
+            } else if (data === -4) {
+                alert("이름을 입력해 주세요.");
+            } else if (data === -5) {
+                alert("닉네임을 입력해 주세요.");
+            } else if (data === -6) {
+                alert("패스워드를 입력해 주세요.");
+            } else if (data === -7) {
+                alert("휴대폰번호를 입력해 주세요.");
+            } else {
+                alert("가입처리 중 오류가 발생했습니다.");
+            }
+        },
+        error : function (error){
+            alert(error);
+        }
+    });
+}

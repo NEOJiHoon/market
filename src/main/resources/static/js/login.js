8a5b2eb0eca906c30053fe112a8fd9e6d8201308
Ji Hoon <gloryhoon@gmail.com>
2022-08-14 17:49:58 +0900

    로그인 페이지 새로운 디자인 적용

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("#Login");
+
+loginForm.addEventListener("submit", login);
+
+function login(event) {
+    event.preventDefault();
+    console.log('login');
+
+    var params = {
+        memId: $('#memId').val(),
+        memPw: $('#memPw').val()
+    }
+
+    $.ajax({
+        type : "POST",
+        url : "/login",
+        contentType : "application/json",
+        data : JSON.stringify(params),
+        success : function(data){
+            console.log('data: ', data);
+            if (data === 'success') {
+                sessionStorage.setItem("id", params.memId);
+                location.href = '/';
+            } else {
+                alert(data);
+            }
+        },
+        error : function(error){
+            alert('로그인오류');
+        }
+    });
+
+}

38f3fc6f0d80a9b6e3220bbd984509a9fe3e4292
Ji Hoon <gloryhoon@gmail.com>
2022-08-15 14:36:27 +0900

    상품등록 페이지 새로운 디자인 적용

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-    $("#main").load("/main/item-list/item-list.html");
+    goItemList();
 });
+
+function goItemList() {
+    $("#main").load("/main/item-list/item-list.html");
+}
 
 function goItemAdd() {
     $("#main").load("/main/item-add/item-add.html");
---
+++ src/main/resources/static/main/item-add/item-add.js
@@ -0,0 +1,60 @@
+function isLogin() {
+    $.ajax({
+        type: "GET",
+        url: "/login/is",
+        success: function(res){
+            console.log("res: ", res);
+            if (res === -1) {
+                alert("권한이 없습니다.");
+                location.href = '/login.html';
+            }
+        }
+    });
+}
+isLogin();
+function cancel() {
+    goItemList();
+}
+
+
+function createItem() {
+    var params = {
+        title: $('#subject').val(),
+        price: $('#price').val(),
+        contents: $('#item-content').val()
+    }
+    const imageInput = $('#image')[0];
+    console.log('imageInput', imageInput.files);
+    if (imageInput.files.length === 0) {
+        alert("파일을 선택해주세요.");
+        return;
+    }
+    const formData = new FormData();
+    formData.append("image", imageInput.files[0]);
+    formData.append("item", JSON.stringify(params));
+
+    $.ajax({
+        type:"POST",
+        url: "/item",
+        processData: false,
+        contentType: false,
+        data: formData,
+        success: function (res) {
+            console.log("res: ", res);
+            if (res === -1) {
+                alert("권한이 없습니다.");
+                location.href = '/login.html';
+            } else {
+                alert("성공");
+                goItemList();
+            }
+        },
+        err: function(err){
+            console.log("err:", err);
+            alert("오류가 발생하였습니다.");
+        }
+    });
+}
+
+
+

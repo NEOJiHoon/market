1cd8b7b54b1304a9b47438e05719f0adae0f6b86
Ji Hoon <gloryhoon@gmail.com>
2022-08-15 17:12:59 +0900

    상품 상세 페이지 새로운 디자인 적용

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,3 +9,12 @@ function goItemList() {
 function goItemAdd() {
     $("#main").load("/main/item-add/item-add.html");
 }
+
+// 글로벌(전역으로 사용하기 위해) 변수 2개 선언
+var g_selected_mem_id;
+var g_selected_item_no;
+function goItemDetail(memId, itemNo) {
+    g_selected_mem_id = memId;
+    g_selected_item_no = itemNo;
+    $("#main").load("/main/item-detail/item-detail.html");
+}

2c0f0b53611f2fbed89c32c85d9bfdf45edabd04
Ji Hoon <gloryhoon@gmail.com>
2022-08-26 16:35:09 +0900

    로그인 오류발생 예외처리 추가 수정

--- src/main/java/com/jihoon/market/controller/ErrorControllerAdvice.java
+++ src/main/java/com/jihoon/market/controller/ErrorControllerAdvice.java
@@ -14,15 +14,15 @@ public class ErrorControllerAdvice {
     @ExceptionHandler(CommonException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST) // HTTP 상태코드: 잘못된 요청 (400)
     @ResponseBody String handleCommonException(CommonException e) {
-        log.error("handleCommonException", e);
-        return e.getMessage();
+        log.info("COMMON EXCEPTION: {}", e.getMessage());  //예외의 메시지만 로깅처리
+        return e.getMessage();  // 예외의 메시지를 응답으로 리턴
     }
 
     @ExceptionHandler({Exception.class}) // Exection은 모든 예외의 부모
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // HTTP 상태코드: 내부 서버 오류 (500)
     @ResponseBody String handleUnknownException(Exception e) {
-        log.error("handleUnknownException", e);
-        return e.getMessage();
+        log.error("handleUnknownException", e);  // 예외를 오류로 로깅처리
+        return e.getMessage(); // 예외의 메시지를 응답으로 리턴
     }
 
 }
--- src/main/java/com/jihoon/market/controller/LoginController.java
+++ src/main/java/com/jihoon/market/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.jihoon.market.controller;
 
 
+import com.jihoon.market.exception.CommonException;
 import com.jihoon.market.mapper.MemberMapper;
 import com.jihoon.market.model.Member;
 import com.jihoon.market.util.SHA256;
@@ -24,16 +25,16 @@ public class LoginController {
         String id = member.getMemId();
         String pw = member.getMemPw();
         if (id == null || "".equals(id.trim())) {
-          throw new RuntimeException("아이디를 정확히 입력하세요.");
+          throw new CommonException("아이디를 정확히 입력하세요.");
          //   return "아이디를 정확히 입력하세요.";
         } else if (pw == null || "".equals(pw.trim())) {
-           throw new RuntimeException("패스워드를 정확히 입력하세요.");
+           throw new CommonException("패스워드를 정확히 입력하세요.");
          //   return "패스워드를 정확히 입력하세요.";
         }
         Member dbMember = memberMapper.selectMember(id);
         String hash = SHA256.encrypt(pw);
         if (dbMember == null || !hash.equals(dbMember.getMemPw())) {
-           throw new RuntimeException("잘못된 로그인 정보입니다.");
+           throw new CommonException("잘못된 로그인 정보입니다.");
          //     return "잘못된 로그인 정보입니다.";
         }
         // 로그인 성공

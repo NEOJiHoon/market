01a5b9505959316e1f632d5c1127b9ac4022ec73
Ji Hoon <gloryhoon@gmail.com>
2022-09-02 17:32:33 +0900

    상품 등록 페이지 이미지 등록 시 미리보기 기능 코드 추가

--- src/main/resources/static/main/item-add/item-add.js
+++ src/main/resources/static/main/item-add/item-add.js
@@ -65,5 +65,58 @@ function createItem() {
     });
 }
 
+// 첫번째 이미지에 변경(이벤트)이 발생한 경우에 수행될 함수를 등록한다.
+$("#image").change(function (e){
+    if (e.target.files.length > 0) {
+        // 선택된 이미지가 있는 경우
+        var reader = new FileReader();
+        // 리더에 이미지 로드 이벤트가 발생할 때 수행될 함수를 등록한다.
+        reader.onload = function (e) {
+            // image-one-display 에 이미지를 변경한다.
+            $("#image-one-display").attr('src', e.target.result);
+        };
+        // 리더로 선택된 이미지를 읽는다. (이미지를 읽음으로 인해 위의 onload 이벤트가 발생함)
+        reader.readAsDataURL(e.target.files[0]);
+    } else {
+        // 선택된 이미지가 없는 경우 (기본이미지(로켓)로 복구)
+        $("#image-one-display").attr('src', '/img/undraw_rocket.svg');
+    }
+});
+
+// 두번째 이미지에 변경(이벤트)이 발생한 경우에 수행될 함수를 등록한다.
+$("#image-two").change(function (e){
+    if (e.target.files.length > 0) {
+        // 선택된 이미지가 있는 경우
+        var reader = new FileReader();
+        // 리더에 이미지 로드 이벤트가 발생할 때 수행될 함수를 등록한다.
+        reader.onload = function (e) {
+            // imager-two-display 에 이미지를 변경한다.
+            $("#image-two-display").attr('src', e.target.result);
+        };
+        // 리더로 선택된 이미지를 읽는다. (이미지를 읽음으로 인해 위의 onload 이벤트가 발생함)
+        reader.readAsDataURL(e.target.files[0]);
+    } else {
+        // 선택된 이미지가 없는 경우 (기본이미지(로켓)로 복구)
+        $("#image-two-display").attr('src', '/img/undraw_rocket.svg');
+    }
+});
+
+// 세번째 이미지에 변경(이벤트)이 발생한 경우에 수행됨 함수를 등록한다.
+$("#image-three").change(function (e) {
+   if (e.target.files.length > 0) {
+       // 선택된 이미지가 있는 경우
+       var reader = new FileReader();
+       // 리더에 이미지 로드 이벤트가 발생할 때 수행될 함수를 등록한다.
+       reader.onload = function (e) {
+           // image-three-display 에 이미지를 변경한다.
+           $("#image-three-display").attr('src', e.target.result);
+       };
+       // 리더로 선택된 이미지를 읽는다. (이미지를 읽음으로 인해 위의 onload 이벤트가 발생함)
+       reader.readAsDataURL(e.target.files[0]);
+   } else {
+       // 선택된 이미지가 없는 경우 (기본이미지(로켓)로 복구)
+       $("#image-three-display").attr('src', '/img/undraw_rocket.svg');
+   }
+});
 
 
